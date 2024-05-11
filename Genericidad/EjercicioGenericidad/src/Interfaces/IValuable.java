0b847cadddd1a9e0181e48bbb735bf9ef8216c89
Leo-Dev <leonardocaimmi1@gmail.com>
2024-05-10 21:58:04 -0300

    Revert "1er vista de genericidad"
    This reverts commit 0ce5333f77138a071b21b2fcc00d982e5e7c6207.

--- Genericidad/EjercicioGenericidad/src/Interfaces/IValuable.java
+++
@@ -1,6 +0,0 @@
-package Interfaces;
-
-public interface IValuable
-{
-    boolean isValuable();
-}
--- Genericidad/EjercicioGenericidad/src/modelo/Zapatilla.java
+++
@@ -1,7 +0,0 @@
-package modelo;
-
-import Interfaces.IValuable;
-
-public class Zapatilla   {
-    ///clase de prueba
-}
